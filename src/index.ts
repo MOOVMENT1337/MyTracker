0642996f7ec0d5e47b96788b62a7598952a6dbe4
MOOVMENT1337 <ezscamalblak52@gmail.com>
2026-09-11 21:46:33 +0300

    DEV-08: Исправил деплой приложения на Vercel (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default } from "../TrackerWebApp/Server/src/vercel.js";
+import express from "express";
+import app from "../TrackerWebApp/Server/src/vercel.js";
+
+export default app as ReturnType<typeof express>;
